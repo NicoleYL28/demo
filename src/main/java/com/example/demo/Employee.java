4958d23d9949d1a3ea1f3c1db0d8e254cd4e630b
TAMNI <tamni@oocl.com>
2025-09-09 21:32:22 +0800

    chore: Initialize Spring Boot project with basic structure and configurations

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private double salary;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getGender(){
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+}
