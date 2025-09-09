1c575fbc93ec9d0b59c367e20ca48c9224d0fe7a
TAMNI <tamni@oocl.com>
2025-09-09 20:09:22 +0800

    test: Add unit tests should_create_employee_when_given_valid_body for EmployeeController

---
+++ src/test/java/com/example/demo/EmployeeControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class EmployeeControllerTest {
+    @Autowired
+    private EmployeeController EmployeeController;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        EmployeeController.employees.clear();
+    }
+
+    @Test
+    void should_create_employee_when_given_valid_body() throws Exception {
+        String requestBody = """
+                { "name": "Tom",
+                "age": "18",
+                "salary": "500.0",
+                "gender": "Male"
+                }
+                """;
+
+        mockMvc.perform(post("/employees")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)).andExpect(status().isCreated()).andExpect(jsonPath("$.id").value(1));
+    }
+
+
+}
